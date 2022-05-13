a992f9d8fa964c393e2bd6b3e592e6554b023ab2
Sivaprathap S <102897756+tringapps-sivaprathap@users.noreply.github.com>
2022-05-13 16:35:20 +0530

    files updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,11 @@ const Card = ({ item }) => {
   return (
     <div className="card">
       <div className="travel-image">
-          <img src={item.imageUrl} alt="travel-image" />
+          <img src={item.imageUrl} alt="traveled-places" />
       </div>
       <div className="travel-details">
           <div><span>{item.location}</span> <a href={item.googleMapsUrl}><span>view on google map</span></a></div>
           <div><h1>{item.title}</h1></div>
-          {/* <div></div> */}
           <div><p><b>{item.startDate} - {item.endDate}</b></p>{item.description}</div>
       </div>
     </div>
